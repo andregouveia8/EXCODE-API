87e4c4b98b6978c74207df2871e4a6820694cfd8
andregouveia8 <afilipecgouveia@gmail.com>
2018-01-06 17:23:58 +0000

    up

---
+++ exCODE API/exCODEAPI/src/com/excode/model/Solution.java
@@ -0,0 +1,28 @@
+package com.excode.model;
+
+public class Solution {
+	
+	public String code;
+	public String language;
+	
+	public Solution(String code, String language) {
+		super();
+		this.code = code;
+		this.language = language;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
+
+}
---
+++ exCODE API/exCODEAPI/src/com/excode/model/TestCase.java
@@ -0,0 +1,10 @@
+package com.excode.model;
+
+public class TestCase {
+	
+	public String input;
+	public String output;
+	
+
+
+}
